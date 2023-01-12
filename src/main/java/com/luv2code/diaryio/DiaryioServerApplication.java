64773fdc6322a066f9d14d7185d351e5466d2e16
lzugaj <lzugaj@croz.net>
2023-01-12 19:59:15 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/diaryio/DiaryioServerApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.diaryio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryioServerApplication.class, args);
+    }
+
+}
